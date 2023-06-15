d015a48376333aa543385a87743a7e00e1f45c88
Yadija <ayuliadi01@gmail.com>
2023-06-15 11:44:33 +0800

    make path users method get

--- src/app.js
+++ src/app.js
@@ -1,32 +1,13 @@
 /* eslint-disable no-console */
 import express from 'express';
-import bcrypt from 'bcrypt';
-import { nanoid } from 'nanoid';
 import 'dotenv/config';
+
+import userRouter from './routes/users.js';
 
 const port = process.env.PORT;
 
 const app = express();
 app.use(express.json());
-
-const users = [];
-
-const userRouter = express.Router();
-userRouter.post('/users', async (req, res) => {
-  const id = `users-${nanoid(15)}`;
-  const { username, password, fullname } = req.body;
-  const hashedPassword = await bcrypt.hash(password, 10);
-
-  users.push({
-    id, username, password: hashedPassword, fullname,
-  });
-  res.status(201).json({
-    status: 'success',
-    data: {
-      userId: id,
-    },
-  });
-});
 
 app.use(userRouter);
 
